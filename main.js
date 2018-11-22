1118077c8688848c90a5307cb75998a7d126be0b
Kuba Sukowski <kubasulek2@gmail.com>
2018-11-22 18:55:33 +0100

    alert animations done

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 //variables
     let sum = 0;
+    let btn2 = document.getElementById("accept-2");
     let pageInfo = document.getElementById("page-info");
     let alertBtn = document.querySelector("#alert-button");
     console.log(pageInfo);
     let checkBoxes = document.querySelectorAll(".section-2 input[type='checkbox']");
     let totalPrice = document.getElementById("price");
-    let form = document.querySelector(".section-2 form");
+    const form = document.querySelector(".section-2 form");
+    const alertPage = document.querySelector(".alert");
     let nodesToArray = [].slice.call( document.querySelectorAll( "input[type='checkbox']" ) ); // change nodeList pseudoArray on array, on wich map(), filter() etc. may operate.
     let priceInputs = nodesToArray.filter(function (e) { return e.hasAttribute("data-price") === true } ); // take out inputs with price attached
     let prices = priceInputs.map(function (e) { return ( parseFloat(e.dataset.price) )}); //take out price values
@@ -15,41 +17,88 @@ document.addEventListener("DOMContentLoaded", function () {
 
 //functions
 
+    //zmiana podstrony
+
+    let changeSlide = function () {
+
+    };
+    //zamkniecie alertu po kliknieciu poza jego boxem
+    let outsideBoxClick = function(){
+        if ( ! alertPage.classList.contains("hide") ){
+            if ( ! event.target.closest(".alert-container") ){showAlert(false)}
+        }
+    };
+
+    //funkcja ktora konczy otwieranie okna alertu, lub zaczyna jego zamykanie
+
     let zoomInOut = function(boolean){
         let element = document.querySelector(".alert-container");
-        let heightUnit = boolean ? 40 : -40;
-        let height = 0;
+        let heightUnit = boolean ? 45 : -45;
+        let height = boolean ? 0 : 800;
         let timer = window.setInterval(function () {
-            if ( height >= 600 ){ window.clearInterval(timer)}
-            element.style.height = height+"px";
-            height += heightUnit;
 
+            if ( boolean && height >= 800 ) {
+                window.clearInterval(timer)
+
+            } else if ( ! boolean && height <= 40 ){
+                element.style.height = "0";
+                fadeOut();
+                window.clearInterval(timer)
+            }
+
+            element.style.height = height + "px";
+            height += heightUnit;
         },10)
     };
-    let fadeInOut = function(boolean){
-      let container = document.querySelector(".alert");
-      let op = boolean ? 0.1 : -0.1;
-      let timer = window.setInterval(function () {
 
-          if ( op >= 0.7){
-              window.clearInterval(timer);
+    //zaczyna otwierac okno alertu
+    let fadeIn = function(boolean){
 
-              window.setTimeout(function () {
-                  container.style.backgroundColor ="rgba(0,0,0,0.6)";
-                  container.style.opacity = "";
-                  zoomInOut(boolean);
-                  },20)
-          }
+        alertPage.classList.remove("hide");
+        let container = document.querySelector(".alert");
+        let op = 0.1;
+        let timer = window.setInterval(function () {
+            if ( op >= 0.7){
+                  window.clearInterval(timer);
+                  window.setTimeout(function () {
+                      container.style.backgroundColor ="rgba(0,0,0,0.7)";
+                      container.style.opacity = "";
+                      zoomInOut(boolean);
+                      },20)
+            }
           container.style.opacity = op;
           op += op * 0.15;
       },10)
     };
 
-    let showAlert = function(boolean){
-        let alert = document.querySelector(".alert");
-        alert.classList.toggle("hide");
-        fadeInOut(boolean);
+    //konczy zamykac okno alertu
+    let fadeOut = function(){
+
+        let container = document.querySelector(".alert");
+        container.style.backgroundColor ="rgba(0,0,0,0.7)";
+        container.style.opacity = "0.7";
+        let op = 0.7;
+        let timer = window.setInterval(function () {
+            if ( op <= 0.1){
+                alertPage.classList.add("hide");
+                container.style.backgroundColor="";
+                op ="0";
+                window.clearInterval(timer);
+            }
+          container.style.opacity = op;
+          op = op - 0.1 * 0.15;
+      },10)
     };
+    // wywoluje lub chowa alert
+    let showAlert = function(boolean){
+
+        boolean ? fadeIn(boolean) : zoomInOut(boolean);
+        //dodaje lub usuwa event ktory zamyka ramke alertu po kliknieciu poza nia
+        boolean ? document.addEventListener("click",outsideBoxClick) : document.removeEventListener("click",outsideBoxClick)
+
+    };
+
+    //funkcja odpowiedzialna za zaznaczanie lub odznaczanie wszystkich checkboxow
     let checkUncheckAll = function(boolean, element){
 
         for(let i = 0; i < checkBoxes.length -1; i++){ checkBoxes[i].checked = boolean}
@@ -65,6 +114,7 @@ document.addEventListener("DOMContentLoaded", function () {
             element.checked = false;
         }, 300)
     };
+    //operacje na checkboxach
     let handleInputs = function(element, index){
 
         let elChecked = element.checked;
@@ -90,7 +140,10 @@ document.addEventListener("DOMContentLoaded", function () {
         sum = sum.toFixed(2);
         totalPrice.innerText = sum;
     };
+
+    //zaakceptowanie skladnikow
     let handleSubmit = function(){
+
         event.preventDefault();
         let info;
         let alright = false;
@@ -117,9 +170,17 @@ document.addEventListener("DOMContentLoaded", function () {
             handleInputs( element, index )
         });
     });
+//event na formularz, plus zablokowanie przycisku na czas animacji(nie chce dzialac na czas!!)
+    form.addEventListener("submit", function () {
+        window.setTimeout( function () {btn2.style.pointerEvents = "none"},0 );
+        window.setTimeout( function () {btn2.style.pointerEvents = ""},800 );
 
-    form.onsubmit = handleSubmit;
+        handleSubmit();
+    });
+
+    //zamkniecie alertu po kliknieciu w przycisk
     alertBtn.addEventListener("click", function () {
         showAlert(false)
-    })
+    });
+
 });
